c1f36397d561cf950c6d7b7586f1f694e9e53d18
Azul <aj.manzano@uniandes.edu.co>
2020-10-22 12:10:15 -0500

    Taller_Typescript1

---
+++ tallerTypescript/dataCourses.ts
@@ -0,0 +1,18 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Practica de enseñanza", "Margarita Botero", 3),
+  new Course("TI en las organizaciones", "Ricardo Gomez", 3),
+  new Course("Introduccion a la teoria computacional", "John Goodrick", 3),
+  new Course("Sistemas transaccionales", "Anamaria Mojica", 3),
+  new Course("Desarrollo de SW", "Sergio Velasquez", 3),
+  new Course("Ingles 7", "Jhonny Flaco", 0),
+  new Course("Ingles 8", "Jhonny Flaco", 0),
+  new Course("Probabilidad(H)", "Michael Hoegele", 3),
+  new Course("Biologia celular-Teoria", "Felipe Guhl", 3),
+  new Course("Logica", "Dario Garcia", 3),
+  new Course("Seminario de proyecto de grado I", "Monika Winhlmeier", 1),
+  new Course("Pentagono", "Liliana Sanchez", 0),
+  new Course("Compl. Algebra Lineal I", "Azul Manzano", 0),
+  new Course("Biologia celular-Laboratorio", "Adriana Sarmiento", 0)
+]
